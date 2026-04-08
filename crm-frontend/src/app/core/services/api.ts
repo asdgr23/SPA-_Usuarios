8759899f5f123f154e58e87ce7a988e873b33572
asdgr23 <angie.degracia23@gmail.com>
2026-04-08 01:30:57 -0500

    fix: endpoint source error

--- crm-frontend/src/app/core/services/api.ts
+++ crm-frontend/src/app/core/services/api.ts
@@ -1,51 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  // ⚠️ NO poner barra al final
   private API_URL = 'https://spa-usuarios.onrender.com/api';
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
   // ==== Usuarios ====
- getUsers(): Observable<any> {
-  const token = localStorage.getItem('token');
-
-  return this.http.get(`${this.API_URL}/usuarios`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  getUsers(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const url = `${this.API_URL}/usuarios`.replace(/([^:]\/)\/+/g, "$1"); // evita doble slash
+    return this.http.get(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
   }
 
-  deleteUser(id:string):Observable<any>{
+  deleteUser(id: string): Observable<any> {
     const token = localStorage.getItem('token');
-
-    return this.http.delete(`${this.API_URL}/usuarios/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+    const url = `${this.API_URL}/usuarios/${id}`.replace(/([^:]\/)\/+/g, "$1");
+    return this.http.delete(url, {
+      headers: { Authorization: `Bearer ${token}` },
     });
   }
 
   updateUser(id: string, data: any): Observable<any> {
-  const token = localStorage.getItem('token');
-
-  return this.http.put(`${this.API_URL}/usuarios/${id}`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`
-      }
+    const token = localStorage.getItem('token');
+    const url = `${this.API_URL}/usuarios/${id}`.replace(/([^:]\/)\/+/g, "$1");
+    return this.http.put(url, data, {
+      headers: { Authorization: `Bearer ${token}` },
     });
   }
+
   // ==== Auth ====
-  login(data : any) : Observable<any>{
-    return this.http.post(`${this.API_URL}/auth/login`, data);
+  login(data: any): Observable<any> {
+    const url = `${this.API_URL}/auth/login`.replace(/([^:]\/)\/+/g, "$1");
+    return this.http.post(url, data);
   }
 
-  register(data : any) : Observable<any>{
-   return this.http.post(`${this.API_URL}/auth/register`, data);
+  register(data: any): Observable<any> {
+    const url = `${this.API_URL}/auth/register`.replace(/([^:]\/)\/+/g, "$1");
+    return this.http.post(url, data);
   }
 }
